5d9d4a6df3ccc54e6c4efc295690ecd4b2459475
Kwntwl <47460944+Kwntwl@users.noreply.github.com>
2019-07-06 02:34:19 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+$member_token = "NjIwODk2NDkyNjk4OTMtLXx8LS1hNzY3Y2FjMGJhZGYwNzI3MjdlMTc5ZjFmZjg1YzZjNg==";
+$device_id = "0d5492ca38bdca98";
+$cookie_cfduid = "dc52514d96f6ec1b78eca17273ca5feb31562061757";
+$cookie_PHPSESSID = "sn96qle393euceh3d3rh9uehdq";
+$lang = "indonesian";
